379ae4a569db1186a2b4693e67747270fb8f11ac
tls1235 <yotam.mini3@gmail.com>
2026-05-02 22:16:34 +0300

    new hloc

--- javaprojects/hloc/Main.java
+++ javaprojects/hloc/Main.java
@@ -1,11 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
-    String[] input = args[0].split("\n");
+    String[] input = new String[0];
+    try {
+      input = Files.readAllLines(Paths.get("/home/tls123/javaprojects/hloc/.homedb")).toArray(new String[0]);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     ArrayList<String> output = new ArrayList<>();
     for (int i = 0; i < input.length; i++) {
-      if (input[i].substring(input[i].lastIndexOf("/")).contains(args[1])) {
+      if (input[i].substring(input[i].lastIndexOf("/")).contains(args[0])) {
         output.add(input[i]);
       }
     }
